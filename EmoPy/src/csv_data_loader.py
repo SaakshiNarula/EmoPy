c46cd8883b3c0598f4a3af47d5899b257a81e962
SaakshiNarula <62864994+SaakshiNarula@users.noreply.github.com>
2020-08-25 17:38:17 +0400

    Update csv_data_loader.py

--- EmoPy/src/csv_data_loader.py
+++ EmoPy/src/csv_data_loader.py
@@ -42,8 +42,11 @@ class CSVDataLoader(_DataLoader):
                 label_class = row[self.csv_label_col]
                 print(label_class)
                 print(self.target_emotion_map.keys())
-                print(int(label_class) not in list(self.target_emotion_map.keys()))
-                if int(label_class) not in list(self.target_emotion_map.keys()):
+                try:
+                    print(int(label_class) not in list(self.target_emotion_map.keys()))
+                    if int(label_class) not in list(self.target_emotion_map.keys()):
+                        continue
+                except:
                     continue
                 label_class = self.target_emotion_map[label_class]
                 if label_class not in emotion_index_map.keys():
